892f9633c21ba1cf0bb58e672e84b04d650fd8a9
SiaGiann <siayiannaki@gmail.com>
2018-02-18 22:00:03 +0100

    Finish exercise

--- yoga_store.rb
+++ yoga_store.rb
@@ -1,4 +1,5 @@
-shopping_cart = []
+@shopping_cart = []
+@sum = 0
 
 @products = [
   {number: 1, name: "t-shirt", price: 20},
@@ -6,20 +7,45 @@ shopping_cart = []
   {number: 3, name: "Yoga leggings", price: 30},
   {number: 4, name: "Meditation cushion", price: 40}
 ]
-
-puts "These are our products:\n"
+def display_products
+  puts "These are our products:\n"
 
   @products.each do |product|
-    puts "Product No#{product[:number]}-#{product[:name]}-#{product[:price]} euro"
+    puts "#{product[:number]}. #{product[:name]} (#{product[:price]} euro)"
     puts "**" * 20
   end
+end
 
-  
 def select_product
   puts "Please select number!"
   select = gets.to_i
+  selection = @products.select { |product| product[:number] == select }
+  return selection[0]
 end
 
-def add_to_cart(number)
+def add_to_cart(product)
+  @shopping_cart << product
+  puts "Your shopping cart contains: #{@shopping_cart}"
+end
 
+def total_price(product)
+  @sum = @sum + product[:price]
+  puts "The total price of your basket is #{@sum} euro."
+end
+
+display_products
+
+loop do
+  puts "Do you want to continue shopping?(Y/N)"
+  answer = gets.chomp.capitalize
+  if answer == "N"
+    puts "Bye!"
+    break
+  elsif answer == "Y"
+    selection = select_product
+    add_to_cart(selection)
+    total_price(selection)
+  else
+    puts "Wrong choice, please choose Y or N."
+  end
 end
